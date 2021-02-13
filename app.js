98e6d01690936b9a0263ea49c1610860732072ad
AhmadAbuyahya <Ahmadabuyahya998@gmail.com>
2021-02-13 19:55:52 +0200

    problem4 solved and edited problem3

--- app.js
+++ app.js
@@ -54,9 +54,9 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sumAndMultiply(a, b, c) {
   //eslint-disable-line
-  let sum=a+b+c;
-  let multi=a*b*c;
-  return [sum,multi,a+' and '+b+' and '+c+' sum to '+sum+'.','The product of '+a+' and '+b+' and '+c+' is '+multi+'.']
+  let sum3=sum(sum(a,b)[0],c)[0];
+  let multi3=multiply(multiply(a,b)[0],c)[0];
+  return [sum3,multi3,a+' and '+b+' and '+c+' sum to '+sum3+'.','The product of '+a+' and '+b+' and '+c+' is '+multi3+'.']
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
@@ -79,11 +79,13 @@ let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) {
   //eslint-disable-line
+  let sum4=sum(testArray[0], sum(testArray[1], testArray[2])[0])[0];
+  return [sum4, testArray + ' was passed in as an array of numbers, and ' + sum4 + ' is their sum.']
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
